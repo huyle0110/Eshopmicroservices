1111b86188eaa0bb24d62fe9a5e3b8a2ec0ad10c
Huy Le Ngoc (T) <hlengoc@deloitte.ca>
2024-09-19 17:44:34 +0700

    Add get product

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+namespace CatalogAPI.Products.GetProducts
+{
+
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<Product> Products);
+    public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
