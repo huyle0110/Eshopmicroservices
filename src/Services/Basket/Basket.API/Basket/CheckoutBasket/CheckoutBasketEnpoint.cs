d463d46ee7d8ea3ad7578079f1e8ba4e01dc4cc3
Huy Le <lehuy.paul@gmail.com>
2024-12-02 23:54:07 +0700

    Fix wrong name

--- src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketEnpoint.cs
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketEnpoint.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Basket.API.Basket.CheckoutBasket
 {
-    public record CheckoutBasketRequest(BasketCheckoutDto basketCheckoutDto);
+    public record CheckoutBasketRequest(BasketCheckoutDto BasketCheckoutDto);
     public record CheckoutBasketResponse(bool IsSuccess);
     public class CheckoutBasketEnpoint : ICarterModule
     {
